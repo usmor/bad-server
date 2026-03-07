6d672e65f8d6aced36b73d0b5e201dcc8834f00f
usmor <7887330@gmail.com>
2026-03-07 21:11:17 +0300

    Исправляет использование role guard

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import NotFoundError from '../errors/not-found-error'
 
-import auth from '../middlewares/auth'
+import auth, { roleGuardMiddleware } from '../middlewares/auth'
 import authRouter from './auth'
 import customerRouter from './customers'
 import orderRouter from './order'
 import productRouter from './product'
 import uploadRouter from './upload'
+import { Role } from '../models/user'
 
 const router = Router()
 
@@ -14,7 +15,7 @@ router.use('/auth', authRouter)
 router.use('/product', productRouter)
 router.use('/order', auth, orderRouter)
 router.use('/upload', auth, uploadRouter)
-router.use('/customers', auth, customerRouter)
+router.use('/customers', auth, roleGuardMiddleware(Role.Admin), customerRouter)
 
 router.use((_req: Request, _res: Response, next: NextFunction) => {
     next(new NotFoundError('Маршрут не найден'))
